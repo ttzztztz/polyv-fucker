a8a07e85e5fc6dd36a939cab3be036f993b1b4d3
ttzztztz <ttzztztz@outlook.com>
2020-02-17 12:28:25 +0800

    Initian Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## useage
+
+#### 1. Install dependencies
+
+A node.js runtime environment is required. exec `yarn` or `npm install`
+
+#### 2. configure
+
+exec `cp ./config-example.json ./config.json`, then `vim ./config.json`
+
+- set room url: your should replace `??????` with real room id
+- copy cookie: you should first visit the website, setup your real name, and then refresh the page, copy the cookie to the config.json
+- if you want to see more info, set `debug` area to be `true`, otherwise, `false`
+
+#### 3. run
+
+exec `npm run start` or `yarn start`
+
+#### 4. disclaimer
+
+this scripts is written to learn node.js features, please don't run it for illegal usages or you should bear the consequences.
---
+++ main.js
@@ -0,0 +1,94 @@
+const fetch = require("node-fetch");
+const fs = require("fs");
+const { JSDOM } = require("jsdom");
+const io = require("socket.io-client");
+
+(async () => {
+  const config_raw = fs.readFileSync("./config.json");
+  const config = JSON.parse(config_raw);
+
+  const req = await fetch(config.url, {
+    headers: {
+      cookie: config.cookie
+    }
+  });
+  const res_body = await req.text();
+  const dom = new JSDOM(res_body, {
+    runScripts: "dangerously"
+  });
+  const { chatData } = dom.window;
+  console.log(
+    `登陆成功，您加入了${chatData.teacherInfo.nick} ${chatData.teacherInfo.actor}的课程！`
+  );
+  const { userName, userId, chatToken, channelId } = chatData;
+  console.log(
+    `您的用户名${userName}, userid: ${userId}, chatToken: ${chatToken}`
+  );
+
+  const socket = io(`https://chat.polyv.net`, {
+    query: {
+      token: chatToken,
+      version: "2.0",
+      EIO: "3",
+      transport: "websocket"
+    }
+  });
+
+  socket.on("connect", () => {
+    console.log("WebSocket 已经建立");
+  });
+  socket.on("message", data => {
+    if (config.debug) {
+      console.error(data);
+    }
+
+    try {
+      const parsed = JSON.parse(data);
+      if (parsed.EVENT === "SIGN_IN") {
+        console.log(data);
+        const {
+          roomId,
+          data: { message }
+        } = parsed;
+        console.log(`收到签到${roomId}, ${message}`);
+
+        socket.emit(
+          "message",
+          JSON.stringify({
+            EVENT: "TO_SIGN_IN",
+            roomId: roomId,
+            checkinId: message, // todo: is this a string type?
+            user: { userId: userId, nick: userName }
+          })
+        );
+
+        console.log(`签到完毕`);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  });
+  socket.on("disconnect", () => {
+    console.log("socket 连接失败 正在重启！");
+    socket.open();
+  });
+  socket.on("error", err => {
+    console.error(err);
+    socket.open();
+  });
+
+  socket.emit(
+    "message",
+    JSON.stringify({
+      EVENT: "LOGIN",
+      values: [
+        userName,
+        "//livestatic.videocc.net/v_536/assets/wimages/missing_face.png",
+        new Date().getTime()
+      ],
+      roomId: channelId,
+      type: "slice",
+      authorization: ""
+    })
+  );
+})();
